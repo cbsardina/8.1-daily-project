7473eb80bd58506b1d5fc72d0b37ea03250803bd
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-08-02 17:30:45 -0400

    8/1 Daily Proj: Issues with styling. CSS sheet is doing nothing

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log(customers);
 const main = document.querySelector('.main');
   let headerH1 = document.createElement('h1');
   headerH1.textContent = "INTERNAL COMPANY DIRECTORY";
-  headerH1.setAttribute('class', 'heading');
+  headerH1.setAttribute('id', 'heading');
   main.appendChild(headerH1);
 
 console.log(headerH1);
@@ -12,46 +12,52 @@ for(let i = 0; i < customers.results.length; i++){
    let customerInfo = customers.results[i];
 
    let contactContainer = document.createElement('div');
-   let picContainer = document.createElement('img');
-   let nameContainer = document.createElement('p');
+   let picContainer = document.createElement('div');
+   let contactPic = document.createElement('img');
+   let nameContainer = document.createElement('h3');
    let emailContainer = document.createElement('p');
-   let address1Container = document.createElement('p');
-   let address2Container = document.createElement('p');
+   let addressList = document.createElement('ul');
+   let address1 = document.createElement('li');
+   let address2 = document.createElement('li');
    let phoneContainer = document.createElement('p');
    let ssnContainer = document.createElement('p');
 
-   picContainer.style.background = customerInfo.picture.medium; //LEFT OFF HERE FIGURING OUT HOW TO INSERT THE PIC 
+   contactPic.style.background = customerInfo.picture.large;
    nameContainer.textContent = customerInfo.name.first + " " + customerInfo.name.last;
    emailContainer.textContent = customerInfo.email;
-   address1Container.textContent = customerInfo.location.street;
-   address2Container.textContent = customerInfo.location.city + ", " +    customerInfo.location.state + " " + customerInfo.location.postcode;
+   address1.textContent = customerInfo.location.street;
+   address2.textContent = customerInfo.location.city + ", " +    customerInfo.location.state + " " + customerInfo.location.postcode;
    phoneContainer.textContent = customerInfo.cell;
    ssnContainer.textContent = customerInfo.id.value;
 
-   contactContainer.setAttribute('class', 'contactContainer');
-   picContainer.setAttribute('href', customerInfo.picture.medium);
-   nameContainer.setAttribute('class', 'nameContainer');
-   emailContainer.setAttribute('class', 'emailContainer');
-   address1Container.setAttribute('class', 'address1Container');
-   address2Container.setAttribute('class', 'address2Container');
-   phoneContainer.setAttribute('class', 'phoneContainer');
-   ssnContainer.setAttribute('class', 'ssnContainer');
+   contactContainer.setAttribute('id', 'contactContainer');
+   picContainer.setAttribute('id', 'picContainer');
+   contactPic.setAttribute('src', customerInfo.picture.medium);
+   nameContainer.setAttribute('id', 'nameContainer');
+   emailContainer.setAttribute('id', 'emailContainer');
+   addressList.setAttribute('id', 'addressList');
+   address1.setAttribute('id', 'address1');
+   address2.setAttribute('id', 'address2');
+   phoneContainer.setAttribute('id', 'phoneContainer');
+   ssnContainer.setAttribute('id', 'ssnContainer');
 
+   picContainer.appendChild(contactPic);
    contactContainer.appendChild(picContainer);
    contactContainer.appendChild(nameContainer);
    contactContainer.appendChild(emailContainer);
-   contactContainer.appendChild(address1Container);
-   contactContainer.appendChild(address2Container);
+   contactContainer.appendChild(addressList);
+   addressList.appendChild(address1);
+   addressList.appendChild(address2);
    contactContainer.appendChild(phoneContainer);
    contactContainer.appendChild(ssnContainer);
    main.appendChild(contactContainer);
 
-   console.log(picContainer);
-   console.log(nameContainer);
-   console.log(emailContainer);
-   console.log(address1Container);
-   console.log(address2Container);
-   console.log(phoneContainer);
-   console.log(ssnContainer);
-   console.log("-------------------");
+  //  console.log(picContainer);
+  //  console.log(nameContainer);
+  //  console.log(emailContainer);
+  //  console.log(addressList);
+  // //  console.log(address2);
+  //  console.log(phoneContainer);
+  //  console.log(ssnContainer);
+  //  console.log("-------------------");
 }
