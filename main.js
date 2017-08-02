20d24daf5b9b3e6ef1a329f7972ba4702bbfdf0c
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-08-01 23:59:25 -0400

    8/1 Daily Proj: Loop complete. All info iterating on to the page except images. Formatting CSS still to do

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 console.log(customers);
 
 const main = document.querySelector('.main');
+  let headerH1 = document.createElement('h1');
+  headerH1.textContent = "INTERNAL COMPANY DIRECTORY";
+  headerH1.setAttribute('class', 'heading');
+  main.appendChild(headerH1);
 
-// for(let i = 0; i < customers.length; i++){
-   let customerInfo = customers.results[1];
+console.log(headerH1);
+
+for(let i = 0; i < customers.results.length; i++){
+   let customerInfo = customers.results[i];
 
    let contactContainer = document.createElement('div');
-   let headerH1 = document.createElement('h1');
-   let picContainer = document.createElement('div');
+   let picContainer = document.createElement('img');
    let nameContainer = document.createElement('p');
    let emailContainer = document.createElement('p');
    let address1Container = document.createElement('p');
@@ -15,8 +20,7 @@ const main = document.querySelector('.main');
    let phoneContainer = document.createElement('p');
    let ssnContainer = document.createElement('p');
 
-   headerH1.textContent = "INTERNAL COMPANY DIRECTORY";
-   picContainer.textContent = customerInfo.picture.medium;
+   picContainer.style.background = customerInfo.picture.medium; //LEFT OFF HERE FIGURING OUT HOW TO INSERT THE PIC 
    nameContainer.textContent = customerInfo.name.first + " " + customerInfo.name.last;
    emailContainer.textContent = customerInfo.email;
    address1Container.textContent = customerInfo.location.street;
@@ -24,7 +28,24 @@ const main = document.querySelector('.main');
    phoneContainer.textContent = customerInfo.cell;
    ssnContainer.textContent = customerInfo.id.value;
 
-   console.log(headerH1);
+   contactContainer.setAttribute('class', 'contactContainer');
+   picContainer.setAttribute('href', customerInfo.picture.medium);
+   nameContainer.setAttribute('class', 'nameContainer');
+   emailContainer.setAttribute('class', 'emailContainer');
+   address1Container.setAttribute('class', 'address1Container');
+   address2Container.setAttribute('class', 'address2Container');
+   phoneContainer.setAttribute('class', 'phoneContainer');
+   ssnContainer.setAttribute('class', 'ssnContainer');
+
+   contactContainer.appendChild(picContainer);
+   contactContainer.appendChild(nameContainer);
+   contactContainer.appendChild(emailContainer);
+   contactContainer.appendChild(address1Container);
+   contactContainer.appendChild(address2Container);
+   contactContainer.appendChild(phoneContainer);
+   contactContainer.appendChild(ssnContainer);
+   main.appendChild(contactContainer);
+
    console.log(picContainer);
    console.log(nameContainer);
    console.log(emailContainer);
@@ -32,52 +53,5 @@ const main = document.querySelector('.main');
    console.log(address2Container);
    console.log(phoneContainer);
    console.log(ssnContainer);
-// // }
-// =======================================================
-//    newNameTitle.textContent = personInfo.name
-//    newHeightLabel.textContent = 'height ' + personInfo.height
-//    newAgeLabel.textContent = 'age: ' + personInfo.age
-//
-//    personContainer.setAttribute('class', 'person-info')
-//    // <div class="person-info">
-//    //    <h1>Persons Name</h1>
-//    //    <p>Persons Height</p>
-//    //    <p>Persons Age</p>
-//    //     <ul>
-//    //         <li> hobby data</li>
-//    //         <li> hobby data</li>
-//    //
-//    //      </ul>
-//    //
-//    // </div>
-//     personContainer.style.color = personInfo.favoriteColor
-//    personContainer.appendChild(newNameTitle)
-//    personContainer.appendChild(newHeightLabel)
-//    personContainer.appendChild(newAgeLabel)
-//    personContainer.appendChild(newHobbyList)
-//    appContainer.appendChild(personContainer)
-//    }
-
-// }
-
-
-// let hobbyListItem = document.createElement('li')
-//       hobbyListItem.textContent = hobby
-//       console.log('------')
-//       console.log(hobbyListItem)
-//       console.log('------')
-//       let newHobbyList = document.createElement('ul');
-//       for(let n = 0; n < personInfo.hobbies.length; n++) {
-//          let hobby = personInfo.hobbies[n]
-//          let hobbyListItem = document.createElement('li')
-//          hobbyListItem.textContent = hobby
-//          console.log('------')
-//          console.log(hobbyListItem)
-//          console.log('------')
-//
-//          // console.log(personInfo.name);
-//
-//          // console.log(hobby)
-//          newHobbyList.appendChild(hobbyListItem)
-//       }
-//       console.log(newHobbyList)
+   console.log("-------------------");
+}
